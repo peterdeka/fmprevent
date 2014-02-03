d6f2d8dd5eec77a8448805f38410fc458ad19b02
peterdeka <pietro.decaro@gmail.com>
2014-02-03 17:26:01 +0100

    Interaction ok, done 4 and 5. Got to work on serialization and WP plugin integration.

--- js/script.js
+++ js/script.js
@@ -1,100 +1,106 @@
 (function(){
 
-FMPrevent={};
-render={};
-FMPrevent.Models={};
-FMPrevent.Collections={};
-FMPrevent.Views={};
-FMPrevent.Models.Connector = Backbone.Model.extend();
+    FMPrevent={};
+    render={};
+    FMPrevent.Models={};
+    FMPrevent.Collections={};
+    FMPrevent.Views={};
+    FMPrevent.Models.Connector = Backbone.Model.extend();
 
+    Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b) // Or === depending on your needs
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
 
-FMPrevent.Models.CableEnd = Backbone.Model.extend({
+    FMPrevent.Models.CableEnd = Backbone.Model.extend({
 
-	defaults:{
+       defaults:{
 
-		side:'n',
-		type:'n',
-		n_conns:0,
-		conns:[]
-	},
+          side:'n',
+          type:'n',
+          n_conns:0,
+          conns:[]
+      },
 
-    initialize: function(){
+      initialize: function(){
         var connectors=[];
-         for(i=0;i<this.get('n_conns');i++){
+        for(i=0;i<this.get('n_conns');i++){
             var c=new FMPrevent.Models.Connector({idx:i+1,n_conns:this.get('n_conns'),side:this.get('side'),type:'puntale',label:''});
             connectors.push(c); 
-         }
+        }
         this.set('connectors',connectors);
     }
 
 });
 
-FMPrevent.Models.Cable = Backbone.Model.extend({
+    FMPrevent.Models.Cable = Backbone.Model.extend({
 
-		initialize: function(){
+      initialize: function(){
 
-            var a=this.get('type').split('-');
-            this.set('n_wires',parseInt(a[a.length-1]));
-            this.set('right_end',new FMPrevent.Models.CableEnd({side:'r',type:'freecables',n_conns:this.get('n_wires'),conns:[]}));
-            this.set('left_end',new FMPrevent.Models.CableEnd({side:'l',type:'freecables',n_conns:this.get('n_wires'),conns:[]}));
-
-        }
-
-	});
-
-
-
-FMPrevent.Views.Connector = Backbone.View.extend({
-
-    events:{
-
-        "input .conn-label" : "change_label",
-        "change .conn-selector" : "change_conn_type"
-    },
-
-    initialize: function(){
-
-
-    },
-
-    render: function(){
-
-        var html = get_and_render('connview',this.model.toJSON());
-        this.$el.html(html);
-        return this;
-    },
-
-    change_label: function(){
-
-        this.model.set('label',this.$el.find('.conn-label').val());
-
-    },
-
-    change_conn_type: function(){
-
-        this.model.set('type',this.$el.find('.conn-selector').val());
-        this.render();
+        var a=this.get('type').split('-');
+        this.set('n_wires',parseInt(a[a.length-1]));
+        this.set('right_end',new FMPrevent.Models.CableEnd({side:'r',type:'freecables',n_conns:this.get('n_wires'),conns:[]}));
+        this.set('left_end',new FMPrevent.Models.CableEnd({side:'l',type:'freecables',n_conns:this.get('n_wires'),conns:[]}));
 
     }
 
 });
 
 
-FMPrevent.Views.CableEnd = Backbone.View.extend({
 
-    events: {
-      
-      "input .cable-sgua"   : "change_end_sgua"
+    FMPrevent.Views.Connector = Backbone.View.extend({
 
-    },
+        events:{
 
-   	initialize: function(){
-		
+            "input .conn-label" : "change_label",
+            "change .conn-selector" : "change_conn_type"
+        },
 
-    },
+        initialize: function(){
 
-    render: function() {
-    	
+
+        },
+
+        render: function(){
+
+            var html = get_and_render('connview',this.model.toJSON());
+            this.$el.html(html);
+            return this;
+        },
+
+        change_label: function(){
+
+            this.model.set('label',this.$el.find('.conn-label').val());
+
+        },
+
+        change_conn_type: function(){
+
+            this.model.set('type',this.$el.find('.conn-selector').val());
+            this.render();
+
+        }
+
+    });
+
+
+    FMPrevent.Views.CableEnd = Backbone.View.extend({
+
+        events: {
+
+          "input .cable-sgua"   : "change_end_sgua"
+
+      },
+
+      initialize: function(){
+
+
+      },
+
+      render: function() {
+
         var html = get_and_render('freecables',this.model.toJSON());
         this.$el.html(html);
         var me=this.$el.find('div.conn-container');
@@ -103,7 +109,7 @@ FMPrevent.Views.CableEnd = Backbone.View.extend({
             me.append(v.render().$el);
         });
         return this;
-		
+
     },
 
     change_end_sgua: function(){
@@ -115,57 +121,64 @@ FMPrevent.Views.CableEnd = Backbone.View.extend({
 
 
 
-FMPrevent.Views.Cable = Backbone.View.extend({
+    FMPrevent.Views.Cable = Backbone.View.extend({
 
-	 el: "#fmprevent",
+      el: "#fmprevent",
 
-     events: {
-      "input #cable-type"   : "change_cable_model",
+      events: {
+      "autocompleteselect #cable-type"   : "change_cable_model",
       "input #cable-sig-l"   : "change_label_l",
       "input #cable-sig-r"   : "change_label_r",
       "input #cable-length"   : "change_cable_length",
-        
-    },
 
-    initialize: function(){
-		
-		this.render();
-    },
+  },
 
-    render: function() {
+  initialize: function(){
 
-        var html = get_and_render('cableview',null);
-        this.$el.html(html);
-        var vr=new FMPrevent.Views.CableEnd({model:this.model.get('right_end')});
-        this.$el.append(vr.render().$el);
-        var vl=new FMPrevent.Views.CableEnd({model:this.model.get('left_end')});
-        this.$el.append(vl.render().$el);
-    },
+      this.render();
+  },
 
-    change_cable_model:  function(){
-        var gigio=2;
-    },
+  render: function() {
 
-    change_label_l: function(){
+    var html = get_and_render('cableview',null);
+    this.$el.html(html);
+    var vr=new FMPrevent.Views.CableEnd({model:this.model.get('right_end')});
+    this.$el.append(vr.render().$el);
+    var vl=new FMPrevent.Views.CableEnd({model:this.model.get('left_end')});
+    this.$el.append(vl.render().$el);
+    var me=this;
+    this.$el.find("#cable-type").autocomplete({
+        source: cable_types
+     }).val(this.model.get('type'));
+},
 
-        this.model.set('label_l',this.$el.find("#cable-sig-l").val());
-        
-    },
+change_cable_model:  function(e, ui){
+    
+    this.model=new FMPrevent.Models.Cable({type:ui.item.value});
+    this.render();
+},
 
-    change_label_r: function(){
+change_label_l: function(){
 
-        this.model.set('label_r',this.$el.find("#cable-sig-r").val());
+    this.model.set('label_l',this.$el.find("#cable-sig-l").val());
 
-    },
+},
 
-    change_cable_length: function(){
+change_label_r: function(){
 
-        this.model.set('t_length',this.$el.find("#cable-length").val());
+    this.model.set('label_r',this.$el.find("#cable-sig-r").val());
 
-    },
+},
+
+change_cable_length: function(){
+
+    this.model.set('t_length',this.$el.find("#cable-length").val());
+
+},
 });
 
 cable_types=['HEF-3','HEF-4','HEF-5'];
+
 thecable = new FMPrevent.Views.Cable({model:new FMPrevent.Models.Cable({type:cable_types[0]})});
 
 })();
